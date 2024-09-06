5a40a1137dc7fd05119023380db427571d5df18f
MarioGranada <mario.granada190@gmail.com>
2024-09-06 11:24:05 -0700

    feat: use mongodb on given requests

--- app/api/comments/[eventId]/route.js
+++ app/api/comments/[eventId]/route.js
@@ -1,5 +1,8 @@
-export async function POST(request) {
+import { MongoClient } from 'mongodb';
+
+export async function POST(request, { params }) {
   const body = await request.json();
+  const { eventId } = params;
 
   const { email, name, text } = body;
 
@@ -15,18 +18,33 @@ export async function POST(request) {
     });
   }
 
-  console.log('data here', { email, name, text });
+  // const newComment = {
+  //   id: new Date().toString(),
+  //   email,
+  //   name,
+  //   text,
+  // };
+
   const newComment = {
-    id: new Date().toString(),
     email,
     name,
     text,
+    eventId,
   };
+
+  console.log('data here', { newComment });
+
+  const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfrndqf.mongodb.net/newsletter-nextjs-course?retryWrites=true&w=majority&appName=Cluster0`;
+  const client = await MongoClient.connect(dbUrl);
+  const db = client.db();
+  const result = await db.collection('comments').insertOne(newComment);
+  console.log('in here oe result', { result });
+  client.close();
 
   return new Response(
     JSON.stringify({
       message: 'Comment added Successfully!',
-      comment: newComment,
+      comment: { ...newComment, id: result.insertedId },
     }),
     {
       status: 201,
@@ -34,26 +52,46 @@ export async function POST(request) {
   );
 }
 
-export async function GET(request) {
-  const dummyList = [
-    {
-      id: 'c1',
-      name: 'sarah',
-      text: 'this is a first comment',
-    },
-    {
-      id: 'c2',
-      name: 'Maximilian',
-      text: 'this is a second comment',
-    },
-    {
-      id: 'c3',
-      name: 'Peter',
-      text: 'this is a third comment',
-    },
-  ];
+export async function GET(request, { params }) {
+  const { eventId } = params;
+  // const dummyList = [
+  //   {
+  //     id: 'c1',
+  //     name: 'sarah',
+  //     text: 'this is a first comment',
+  //   },
+  //   {
+  //     id: 'c2',
+  //     name: 'Maximilian',
+  //     text: 'this is a second comment',
+  //   },
+  //   {
+  //     id: 'c3',
+  //     name: 'Peter',
+  //     text: 'this is a third comment',
+  //   },
+  // ];
 
-  return new Response(JSON.stringify({ comments: dummyList }), {
-    status: 200,
-  });
+  // return new Response(JSON.stringify({ comments: dummyList }), {
+  //   status: 200,
+  // });
+
+  const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfrndqf.mongodb.net/newsletter-nextjs-course?retryWrites=true&w=majority&appName=Cluster0`;
+  const client = await MongoClient.connect(dbUrl);
+  const db = client.db();
+  const result = await db
+    .collection('comments')
+    .find({ eventId: eventId })
+    .sort({ _id: -1 })
+    .toArray();
+  client.close();
+
+  return new Response(
+    JSON.stringify({
+      comments: result.map((item) => ({ ...item, id: item._id })),
+    }),
+    {
+      status: 200,
+    }
+  );
 }
--- app/api/newsletter/route.js
+++ app/api/newsletter/route.js
@@ -1,3 +1,5 @@
+import { MongoClient } from 'mongodb';
+
 export async function POST(request) {
   const body = await request.json();
   // const body = { email: 'this@isatest.com' };
@@ -13,6 +15,15 @@ export async function POST(request) {
   }
 
   console.log('User Email', { message: 'Signed up!', userEmail });
+
+  const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vfrndqf.mongodb.net/newsletter-nextjs-course?retryWrites=true&w=majority&appName=Cluster0`;
+  const client = await MongoClient.connect(dbUrl);
+  // client.db('events');
+
+  const db = client.db();
+  await db.collection('emails').insertOne({ email: userEmail });
+
+  client.close();
 
   const response = new Response(JSON.stringify({ message: 'Signed up!' }), {
     status: 201,
